ef27ac19e1bc4c621e5d428eca63adc09125763a
Noah Mulvaney <mulvaney.np@gmail.com>
2024-03-14 16:14:32 -0600

    Implement Dijkstra's

--- code.js
+++ code.js
@@ -4,13 +4,13 @@
 
 // Implement Dijkstra's Algorithm
 
-// Credit: 
+// Credit: Online resources
+//      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
 
 // Find whether a given value exists in a list -- Theta(V)
 function find(list, value) {
     for (let i = 0; i < list.length; i++) 
         if (list[i] == value) return true;
-    
     return false;
 }
 
@@ -19,14 +19,20 @@ function find(list, value) {
 function findMin(dist, marked) {
     let index;
     let min = Infinity;
-
     for (let i = 0; i < dist.length; i++) 
         if (!find(marked, i) && dist[i] < min) {
             index = i;
             min = dist[i];
         }
-
     return index;
+}
+
+// Flatten nested arrays -- Theta(n)
+function flatten(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) 
+        result.push(arr[i].flat(Infinity));
+    return result;
 }
 
 // Find the shortest path to each node from the source
@@ -34,35 +40,33 @@ function dijkstra(graph, sourceNode) {
     let marked = []; // store marked nodes
     let current = 0;
 
-    // initialize the paths
+    // initialize the paths and distances
     let paths = [];
-    for (let i = 0; i < graph.length; i++)
-        paths.push([]);
-
-    // initialize the distances
     let dist = [];
-    for (let i = 0; i < graph.length; i++)
+    for (let i = 0; i < graph.length; i++) {
+        paths.push([]);
         dist[i] = Infinity;
+    }
+    paths[sourceNode] = [sourceNode];
     dist[sourceNode] = 0;
     
     for (let i = 0; i < graph.length; i++) {
-        // update paths
-        for (let j = 0; j < graph.length; j++)
-            if (!find(marked, j)) paths[j].push(current); // BUG pushes nodes not used in shortest path
-
         // mark current node
         marked.push(current);
 
         // update distances
         for (let j = 0; j < graph.length; j++)
-            if (graph[current][j]) dist[j] = dist[current] + graph[current][j];
-
+            if (graph[current][j] && dist[j] > dist[current] + graph[current][j]) {
+                dist[j] = dist[current] + graph[current][j];
+                paths[j] = [paths[current], j];
+            }
+        
         // find next node
         current = findMin(dist, marked);
     }
 
-    return paths;
+    return flatten(paths);
 }
 
 let graph = [[0, 5, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0], [0, 1, 0, 0, 3, 0], [0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 2], [0, 0, 0, 0, 0, 0]];
-console.log(dijkstra(graph, 0)); // [0, 5, 2, 7, 8, 10]     [[0], [0, 1], [0, 2], [0, 1, 3], [0, 2, 4], [0, 2, 4, 5]]
+console.log(dijkstra(graph, 0)); // [[0], [0, 2, 1], [0, 2], [0, 2, 1, 3], [0, 2, 4], [0, 2, 4, 5]]
