861759b748ca0f22c0adcf7cd89d0d533404e5e6
Noah Mulvaney <127539141+noah-mulvaney@users.noreply.github.com>
2024-03-20 15:41:07 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 // COSC3020 Dijkstra's Algorithm Exercise
 // Noah Mulvaney, nmulvane@uwyo.edu
-// 14 Mar 2024
+// 20 Mar 2024
 
 // Implement Dijkstra's Algorithm
 
 // Credit: Online resources
 //      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
 
-// Find whether a given value exists in a list -- Theta(V)
+// Find whether a given value exists in a list
 function find(list, value) {
     for (let i = 0; i < list.length; i++) 
         if (list[i] == value) return true;
@@ -15,19 +15,19 @@ function find(list, value) {
 }
 
 
-// Find the unmarked node with the lowest distance -- Theta(V^2)
-function findMin(dist, marked) {
+// Find the unmarked node with the lowest distance
+function findMin(dist, unmarked) {
     let index;
     let min = Infinity;
     for (let i = 0; i < dist.length; i++) 
-        if (!find(marked, i) && dist[i] < min) {
+        if (find(unmarked, i) && dist[i] < min) {
             index = i;
             min = dist[i];
         }
     return index;
 }
 
-// Flatten nested arrays -- Theta(n)
+// Flatten nested arrays
 function flatten(arr) {
     let result = [];
     for (let i = 0; i < arr.length; i++) 
@@ -37,32 +37,33 @@ function flatten(arr) {
 
 // Find the shortest path to each node from the source
 function dijkstra(graph, sourceNode) {
-    let marked = []; // store marked nodes
+    let unmarked = []; // store unmarked nodes
     let current = 0;
 
     // initialize the paths and distances
     let paths = [];
     let dist = [];
     for (let i = 0; i < graph.length; i++) {
+        unmarked.push(i);
         paths.push([]);
         dist[i] = Infinity;
     }
     paths[sourceNode] = [sourceNode];
     dist[sourceNode] = 0;
     
-    for (let i = 0; i < graph.length && current != undefined; i++) { // iterates V times
+    for (let i = 0; i < graph.length && current != undefined; i++) {
         // mark current node
-        marked.push(current);
+        unmarked.splice(unmarked.indexOf(current), 1);
 
         // update distances
-        for (let j = 0; j < graph.length; j++) // V times
+        for (let j = 0; j < graph.length; j++)
             if (graph[current][j] && dist[j] > dist[current] + graph[current][j]) {
                 dist[j] = dist[current] + graph[current][j];
                 paths[j] = [paths[current], j];
             }
         
         // find next node
-        current = findMin(dist, marked); // V^2 complexity
+        current = findMin(dist, unmarked);
     }
 
     return flatten(paths);
